644ee2b18762a141e665a0b1ffe5d3353bc68e61
leon <leon@hooc.top>
2020-04-02 18:02:58 +0800

    完成首页

--- src/Page/Welcome.js
+++ src/Page/Welcome.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
-import { Row, Col ,Button} from 'antd';
+import { Row, Col, Button } from 'antd';
+import { SmileOutlined, WhatsAppOutlined } from '@ant-design/icons';
+import RotationChart from '../Components/Home/RotationChart ';
+import { Link } from 'react-router-dom';
 import './Welcome.css';
-function Welcome(){
+function Welcome() {
     return (
         <>
-        <Row type="flex" justify="center" >
-            <Col xs={24} sm={24} md={20} lg={16} xl={16}>
-                      <div className="welcome-panel">
-                          <div className="welcome-content">
-                            <h1>欢迎使用文理点餐平台</h1>
-                            <Button>我是用户</Button>
-                            <Button>我是商家</Button>
-                          </div>
-                      </div>
-            </Col>
-        </Row>
+            <Row type="flex" justify="center" >
+                <Col xs={24} sm={24} md={20} lg={16} xl={16}>
+                    <div className="welcome-panel">
+                        <div className="welcome-content">
+                            <div className="content-header">欢迎使用文理点餐平台</div>
+                            <Link to="/u">
+                                <Button type="primary" size="large"><SmileOutlined />我是用户</Button>
+                            </Link>
+                            <Link to="/a">
+                                <Button type="primary" size="large"><WhatsAppOutlined />我是商家</Button>
+                            </Link>
+                            <RotationChart />
+                        </div>
+                    </div>
+                </Col>
+            </Row>
         </>
     )
 }
